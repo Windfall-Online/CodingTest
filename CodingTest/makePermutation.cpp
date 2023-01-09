37a4bac3f5ade3cebd5805289c954fbf85669c21
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2023-01-10 02:03:11 +0900

    commit

--- CodingTest/makePermutation.cpp
+++ CodingTest/makePermutation.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// n = 총 갯수
+// r = 뽑을 갯수
+// depth = 몇번째를 뽑고있는가?
+// 트리를 만들어서 도식화했을 때 깊이에 해당
+
+
+int v[5] = { 1, 2, 3, 4, 5 };
+
+void printV(int* arr)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void permutation(int n, int r, int depth)
+{
+	// 뽑을 갯수와 몇 번째로 뽑는가가 일치하면 출력 후 리턴
+	if (r == depth)
+	{
+		// 출력
+		printV(v);
+		return;
+	}
+	for (int i = depth; i < n; ++i)
+	{
+		swap(v[i], v[depth]);
+		permutation(n, r, depth + 1);
+		swap(v[i], v[depth]);
+	}
+	return;
+}
+
+
 int main()
 {
 
@@ -20,7 +57,7 @@ int main()
 
 		}
 	}*/
-	{
+	/*{
 		bool visited[4] = { false, };
 
 		for (int i = 1; i < 4; ++i)
@@ -43,5 +80,11 @@ int main()
 			}
 			visited[i] = false;
 		}
+	}*/
+
+	{
+
+		permutation(5, 3, 0);
+
 	}
 }
---
+++ CodingTest/split.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+// 문자열을 특정 문자 단위로 끊어서 배열을 만듬
+// ex) abcdabc를 d로 끊어서 {abc, abc}배열로 만든다
+
+int main()
+{
+
+}
